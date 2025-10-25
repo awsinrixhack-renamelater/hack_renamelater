0709c4e56f1a8d6dedc1bebfa48e08db3ee4516d
al-nf <alankjfung@gmail.com>
2025-10-25 15:43:49 -0700

    updated devenv

--- devenv.nix
+++ devenv.nix
@@ -24,7 +24,10 @@
       exec = "go run .";
     };
     flask-api.exec = "flask --app flask-api/app.py run";
-    react-frontend.exec = "npm start --prefix react-frontend";
+    frontend = {
+      cwd = "frontend";
+      exec = "npm run dev";
+    }
   };
 }
 
