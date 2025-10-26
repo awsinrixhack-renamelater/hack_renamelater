6f33cb0e184c1d9d5e3e7fbb09a78ac82ce0034d
Kimi Mitsunaga <kimimitsunaga@Kimis-MacBook-Air.local>
2025-10-26 07:57:25 -0700

    welcome page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,29 +6,27 @@ import Scoreboard from './pages/Scoreboard.tsx'
 import Login from './pages/Login.tsx'
 import Welcome from './pages/Welcome.tsx'
 
-
-
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Redirect root to welcome page */}
         <Route path="/" element={<Navigate to="/welcome" replace />} />
+
+        {/* Main pages */}
+        <Route path="/welcome" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Homepage />} />
         <Route path="/scoreboard" element={<Scoreboard />} />
-        <Route path="/welcome" element={<Welcome />} />
 
-        <Route path="*" element={<Navigate to="/Login" replace />} />
-
-        {/* this is for connecting Scoreboard with Homepage */}
-        <Route path="/" element={<Homepage />} />
+        {/* Optional: Example of linking Home and Scoreboard */}
         <Route path="/about" element={<Scoreboard />} />
+
+        {/* Catch-all route for undefined paths */}
         <Route path="*" element={<Navigate to="/welcome" replace />} />
       </Routes>
-  </Router>
+    </Router>
   )
 }
 
 export default App
-
-
