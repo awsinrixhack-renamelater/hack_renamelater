89313fedc489e1e4d97549ce3c05cbeb034b61b0
Kaavya Jethwa <98070317+k-jethwa@users.noreply.github.com>
2025-10-26 02:22:08 -0700

    New welcome page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,26 @@
-import { useState } from 'react'
-import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 import './App.css'
 import Homepage from './pages/Homepage.tsx'
 import Scoreboard from './pages/Scoreboard.tsx'
 import SignIn from './pages/Signin.tsx'
+import Welcome from './pages/Welcome.tsx'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/signin" replace />} />
+        <Route path="/" element={<Navigate to="/welcome" replace />} />
+        <Route path="/welcome" element={<Welcome />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/home" element={<Homepage />} />
         <Route path="/scoreboard" element={<Scoreboard />} />
-        <Route path="*" element={<Navigate to="/signin" replace />} />
-
-        {/* this is for connecting Scoreboard with Homepage */}
-        <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<Scoreboard />} />
-        
+        <Route path="*" element={<Navigate to="/welcome" replace />} />
       </Routes>
-    </Router>
+  </Router>
   )
 }
 
